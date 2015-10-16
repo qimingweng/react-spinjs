9c3c4f7aa83b41c2694da335ac20071a990171ed
Qiming Weng <qimingweng@users.noreply.github.com>
2015-10-16 10:14:03 -0400

    slightly more sensible config

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@ import Spinner from 'spin.js';
 
 export default class ReactSpinner extends React.Component {
   static propTypes = {
+    // This object is passed in wholesale as the spinner config
     config: PropTypes.object,
-    color: PropTypes.string.isRequired
+    // This is a quick way to overwrite just the color on the config
+    color: PropTypes.string.isRequired,
   }
   static defaultProps = {
-    color: 'black'
+    config: {},
+    color: 'black',
   }
   componentDidMount() {
     const {color, config} = this.props;
-    const spinConfig = config || {width: 2, radius: 10, length: 7, color};
+    const spinConfig = {
+      // a few sensible defaults
+      width: 2,
+      radius: 10,
+      length: 7,
+      // config will overwrite anything else
+      ...config,
+      // color will overwrite config
+      color,
+    };
 
     this.spinner = new Spinner(spinConfig);
     this.spinner.spin(this.refs.container);
