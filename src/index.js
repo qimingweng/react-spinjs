88e9a2e472ffb802a3b1a25a09495e2b635ff818
Qiming Weng <qimingweng@users.noreply.github.com>
2015-10-16 10:07:32 -0400

    use ref as dom node instead of using react-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React, {PropTypes} from 'react';
-import ReactDOM from 'react-dom';
 import Spinner from 'spin.js';
 
 export default class ReactSpinner extends React.Component {
@@ -15,7 +14,7 @@ export default class ReactSpinner extends React.Component {
     const spinConfig = config || {width: 2, radius: 10, length: 7, color};
 
     this.spinner = new Spinner(spinConfig);
-    this.spinner.spin(ReactDOM.findDOMNode(this.refs.container));
+    this.spinner.spin(this.refs.container);
   }
   componentWillUnmount() {
     this.spinner.stop();
